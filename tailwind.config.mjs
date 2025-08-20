6b504dd5262e89111e246baacaab800879fb98f1
Máté Mészáros (Laptop) <meszmatew@gmail.com>
2025-08-20 20:33:36 +0200

    terms & privacy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
         'sans': ['Inter', 'sans-serif'],
         'ai-glow': '0 0 10px #f0f, 0 0 20px #0ff, 0 0 30px #f0f',
       },
+      transitionTimingFunction: {
+        'bezier': 'cubic-bezier(.53,-0.14,.4,1.28)',
+      },
     },
   },
   plugins: [],
